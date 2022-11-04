985a263a81f21b7e67d3c8177da0389d26dfbb57
JOTARO-S <88357567+JOTARO-S@users.noreply.github.com>
2022-11-04 05:15:41 +0000

    [JavaScript] 5 Finish

--- jQuery-Lesson/app.js
+++ jQuery-Lesson/app.js
@@ -1,3 +1,31 @@
-$(document).ready(function() {
-    $("body").html("<h1>Hello jQuery!!</h1>");
+//box1 {
+//  background-color: #FF0000;
+//  height: 200px;
+//  width: 200px;
+//}
+
+//$(function(){
+//  $('.box1').css({
+//    'background-color': '#0000FF',
+//    'height': '100px'
+//  });
+//});
+
+//$(document).ready(function() {
+// $(".box1").show();
+// $(".box1").css({"background-color":"#0000FF"});
+//});
+
+//$(function(){
+// $(".box1").hide();
+//});
+
+$(function(){
+ $(".box1").slideDown(function(){
+ $(".box1").css({
+ 'background-color': '#0000FF',
+ 'height': '100px'
+ });
+ $(".box1").slideUp();
+ });
 });
